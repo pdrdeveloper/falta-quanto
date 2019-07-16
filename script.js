99df225583a1cacbb7e2068c78181ef78fe3bfae
Pedro Henrique <pedrogamerlouco@outlook.com>
2019-07-15 21:58:06 -0300

    feat: Manifest adicionado, Sistema de dias, meses e anos adicionado e calculo de contribuição por dia.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-let select = document.querySelector('select');
+/* ↓ Validações ↓ */
 
-select.addEventListener('change', function() {
+let categoria = document.querySelector('select#categoriaUsuario')
+let modoTempo = document.querySelector('select#tempoOption')
+
+modoTempo.addEventListener('change', function(){
+  optionTempo = this.selectedOptions[0];
+  textoTempo = optionTempo.value
+  console.log(textoTempo)
+})
+
+categoria.addEventListener('change', function() {
   option = this.selectedOptions[0];
   texto = option.textContent;
   console.log(texto)
@@ -11,7 +20,8 @@ select.addEventListener('change', function() {
   let txtin = document.getElementById('txtMeta')
   let txtin2 = document.getElementById('txtAtual')
   let txtin3 = document.getElementById('txtDinmeta')
-  let txtin4 = document.getElementById('txtTexto')
+  let txtin4 = document.getElementById('txtTempo')
+  let select1 = document.getElementById('tempoOption')
   if (texto === 'Estudos') {
     input.style.display = 'block'
     txtin.innerHTML = 'Qual é a sua meta em relação aos estudos?'
@@ -20,7 +30,8 @@ select.addEventListener('change', function() {
     input3.style.display = 'block'
     txtin3.innerHTML = 'Quanto você precisa atingir?'
     input4.style.display = 'block'
-    txtin4.innerHTML = 'Em quantos meses você precisa atingir?'
+    txtin4.innerHTML = 'Em quanto tempo você precisa atingir?'
+    select1.style.display = 'block'
   } else if (texto === 'Viagem') {
     input.style.display = 'block'
     txtin.innerHTML = 'Para onde você deseja viajar?'
@@ -29,7 +40,8 @@ select.addEventListener('change', function() {
     input3.style.display = 'block'
     txtin3.innerHTML = 'Quanto você precisa atingir?'
     input4.style.display = 'block'
-    txtin4.innerHTML = 'Em quantos meses você precisa atingir?'
+    txtin4.innerHTML = 'Em quanto tempo você precisa atingir?'
+    select1.style.display = 'block'
   } else if (texto === 'Compras') {
     input.style.display = 'block'
     txtin.innerHTML = 'O que você deseja comprar?'
@@ -38,7 +50,8 @@ select.addEventListener('change', function() {
     input3.style.display = 'block'
     txtin3.innerHTML = 'Quanto você precisa atingir?'
     input4.style.display = 'block'
-    txtin4.innerHTML = 'Em quantos meses você precisa atingir?'
+    txtin4.innerHTML = 'Em quanto tempo você precisa atingir?'
+    select1.style.display = 'block'
   } else {
     input.style.display = 'none'
     input2.style.display = 'none'
@@ -52,9 +65,15 @@ function validaInfo() {
     dinheiroAtual = document.getElementById('dinheiroAtual').value
     dinheiroMeta = document.getElementById('dinheiroMeta').value
     tempoMeta = document.getElementById('tempoMeta').value
+    resultadoMeta = dinheiroMeta - dinheiroAtual
+    resultadoMetaDia = (resultadoMeta / tempoMeta).toFixed(2)
+    resultadoMetaMes = (resultadoMeta / (tempoMeta*30)).toFixed(2)
+    resultadoMetaAno = (resultadoMeta / (tempoMeta*365)).toFixed(2)
     resultado = document.getElementById('resultado')
-    option = select.selectedOptions[0];
+    option = categoria.selectedOptions[0];
     texto = option.textContent;
+    optionTempo = modoTempo.selectedOptions[0];
+    textoTempo = optionTempo.value
     if (texto == 'Escolha uma categoria.') {
       resultado.innerHTML = 'Por favor, escolha uma categoria.'
     } else {
@@ -67,32 +86,70 @@ function validaInfo() {
       } else if (tempoMeta == "") {
           resultado.innerHTML = 'Por favor, preencha todos os dados acima.'
       } else {
-        if (dinheiroMeta - dinheiroAtual <= 0) {
+        if (resultadoMeta <= 0) {
           resultado.innerHTML = `Você já atingiu a sua meta.`
         } else {
           if (texto === 'Estudos') {
-            if (tempoMeta > 1) {
-              resultado.innerHTML = `Falta ${dinheiroMeta - dinheiroAtual} pontos para você ${metaUsuario} em ${tempoMeta} meses.`
-            } else {
-              resultado.innerHTML = `Falta ${dinheiroMeta - dinheiroAtual} pontos para você ${metaUsuario} em ${tempoMeta} mês.`
-            }          
+            if (tempoMeta <= 1 && textoTempo == 'dia') {
+              resultado.innerHTML = `Falta ${resultadoMeta} pontos para você ${metaUsuario} em ${tempoMeta} dia.`
+            } else if (tempoMeta > 1 && textoTempo == 'dia') {
+              resultado.innerHTML = `Falta ${resultadoMeta} pontos para você ${metaUsuario} em ${tempoMeta} dias.`
+            } else if (tempoMeta <= 1 && textoTempo == 'mes') {
+              resultado.innerHTML = `Falta ${resultadoMeta} pontos para você ${metaUsuario} em ${tempoMeta} mês.`
+            } else if (tempoMeta > 1 && textoTempo == 'mes') {
+              resultado.innerHTML = `Falta ${resultadoMeta} pontos para você ${metaUsuario} em ${tempoMeta} meses.`
+            } else if (tempoMeta <= 1 && textoTempo == 'ano') {
+              resultado.innerHTML = `Falta ${resultadoMeta} pontos para você ${metaUsuario} em ${tempoMeta} ano.`
+            } else if (tempoMeta > 1 && textoTempo == 'ano') {
+              resultado.innerHTML = `Falta ${resultadoMeta} pontos para você ${metaUsuario} em ${tempoMeta} anos.`
+            }
           } else if (texto === 'Viagem') {
-            if (tempoMeta > 1) {
-              resultado.innerHTML = `Falta R$${dinheiroMeta - dinheiroAtual} para você viajar para ${metaUsuario} em ${tempoMeta} meses.`
-            } else {
-              resultado.innerHTML = `Falta R$${dinheiroMeta - dinheiroAtual} para você viajar para ${metaUsuario} em ${tempoMeta} mês.`
+            if (tempoMeta <= 1 && textoTempo == 'dia') {
+              resultado.innerHTML = `Falta R$${resultadoMeta} para você viajar para ${metaUsuario} em ${tempoMeta} dia.<br>
+              Para atingir a sua meta contribua com R$${resultadoMetaDia} todo dia.`
+            } else if (tempoMeta > 1 && textoTempo == 'dia') {
+              resultado.innerHTML = `Falta R$${resultadoMeta} para você viajar para ${metaUsuario} em ${tempoMeta} dias.<br>
+              Para atingir a sua meta contribua com R$${resultadoMetaDia} todo dia.`
+            } else if (tempoMeta <= 1 && textoTempo == 'mes') {
+              resultado.innerHTML = `Falta R$${resultadoMeta} para você viajar para ${metaUsuario} em ${tempoMeta} mês.<br>
+              Para atingir a sua meta contribua com R$${resultadoMetaMes} todo dia.`
+            } else if (tempoMeta > 1 && textoTempo == 'mes') {
+              resultado.innerHTML = `Falta R$${resultadoMeta} para você viajar para ${metaUsuario} em ${tempoMeta} meses.<br>
+              Para atingir a sua meta contribua com R$${resultadoMetaMes} todo dia.`
+            } else if (tempoMeta <= 1 && textoTempo == 'ano') {
+              resultado.innerHTML = `Falta R$${resultadoMeta} para você viajar para ${metaUsuario} em ${tempoMeta} ano.<br>
+              Para atingir a sua meta contribua com R$${resultadoMetaAno} todo dia.`
+            } else if (tempoMeta > 1 && textoTempo == 'ano') {
+              resultado.innerHTML = `Falta R$${resultadoMeta} para você viajar para ${metaUsuario} em ${tempoMeta} anos.<br>
+              Para atingir a sua meta contribua com R$${resultadoMetaAno} todo dia.`
             }
           } else if (texto === 'Compras') {
-            if (tempoMeta > 1) {
-              resultado.innerHTML = `Falta R$${dinheiroMeta - dinheiroAtual} para você comprar ${metaUsuario} em ${tempoMeta} meses.`
-            } else {
-              resultado.innerHTML = `Falta R$${dinheiroMeta - dinheiroAtual} para você comprar ${metaUsuario} em ${tempoMeta} mês.`
+            if (tempoMeta <= 1 && textoTempo == 'dia') {
+              resultado.innerHTML = `Falta R$${resultadoMeta} para você comprar ${metaUsuario} em ${tempoMeta} dia.<br>
+              Para atingir a sua meta contribua com R$${resultadoMetaDia} todo dia.`
+            } else if (tempoMeta > 1 && textoTempo == 'dia') {
+              resultado.innerHTML = `Falta R$${resultadoMeta} para você comprar ${metaUsuario} em ${tempoMeta} dias.<br>
+              Para atingir a sua meta contribua com R$${resultadoMetaDia} todo dia.`
+            } else if (tempoMeta <= 1 && textoTempo == 'mes') {
+              resultado.innerHTML = `Falta R$${resultadoMeta} para você comprar ${metaUsuario} em ${tempoMeta} mês.<br>
+              Para atingir a sua meta contribua com R$${resultadoMetaMes} todo dia.`
+            } else if (tempoMeta > 1 && textoTempo == 'mes') {
+              resultado.innerHTML = `Falta R$${resultadoMeta} para você comprar ${metaUsuario} em ${tempoMeta} meses.<br>
+              Para atingir a sua meta contribua com R$${resultadoMetaMes} todo dia.`
+            } else if (tempoMeta <= 1 && textoTempo == 'ano') {
+              resultado.innerHTML = `Falta R$${resultadoMeta} para você comprar ${metaUsuario} em ${tempoMeta} ano.<br>
+              Para atingir a sua meta contribua com R$${resultadoMetaAno} todo dia.`
+            } else if (tempoMeta > 1 && textoTempo == 'ano') {
+              resultado.innerHTML = `Falta R$${resultadoMeta} para você comprar ${metaUsuario} em ${tempoMeta} anos.<br>
+              Para atingir a sua meta contribua com R$${resultadoMetaAno} todo dia.`
             }
           }
       }
     }
   }
 }
+
+/* ↓ Load Screen ↓ */
 
 $(window).on("load",function(){
     $(".loader-wrapper").fadeOut(2000);
